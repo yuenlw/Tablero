caedd619e8be832bf0b08b65937363e82c9ece77
Yuen Law <yuen.lw@gmai.com>
2021-02-08 19:47:17 -0600

    ADD template for class Tablero

---
+++ Tablero.py
@@ -0,0 +1,23 @@
+from Ficha import *
+
+class Tablero:
+
+    #Defina aquí los atributos de Tablero
+    
+
+
+    #también va a necesitar una lista de Fichas (puede asumir un número de Fichas fijo si le parece más fácil), 
+    #y un mecanismo para saber quién sigue en el turno
+
+
+
+
+    #agregue parámetros necesarios después de self
+    #para iniciar los valores de sus atributos
+    def __init__(self):
+        #inicialice aquí todos los atributos
+        #no olvide usar self.atributo para acceder el atributo de la clase
+
+
+    #defina aquí los métodos de Tablero
+    #recuerde que el primer parámetro de cada método es siempre self
