c76c0b729f0f2e9909e8ae181a924f3ca6b58c9d
Yuen Law <yuen.lw@gmai.com>
2021-02-08 19:46:56 -0600

    ADD comments

--- Dado.py
+++ Dado.py
@@ -3,6 +3,10 @@ from random import *
 class Dado:
 
     caras=0
+
+    #este atributo no estaba en el modelo explícitamente
+    #pero lo necesitamos para poder crear los números aleatorios
+    #normalmente deberíamos esconderlo para que no sea accesible (encapsulamiento)
     rnd=Random(42)
 
     def __init__(self, numCaras):
--- Ficha.py
+++ Ficha.py
@@ -1,16 +1,21 @@
 from Dado import *
 
 class Ficha:
-
-    dado = Dado(6)
     color = ""
     posicion = 0
 
+    #no era parte del modelo, pero la Ficha necesita usar un dado
+    #este atributo se define cuando definimos relaciones entre clases
+    #lo que veremos más adelante en el curso
+    dado = Dado(6)
+    
+    
     def __init__(self, color):
         self.color = color
         self.posicion = 0
     
     def avanzar(self):
+        #aquí se vuelve claro por qué necesitamos un dado
         pasos = self.dado.lanzar()
         self.posicion += pasos
         print(self.posicion)
